72c610cade8a1df93d95204166cc4ce58ae2121b
SinghHarsh2003 <136444805+SinghHarsh2003@users.noreply.github.com>
2025-05-23 01:19:20 +0530

    05: search-filters

---
+++ multitenant-ecommerce/src/app/(app)/(home)/features/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <div>
+      <p>Feature</p>
+    </div>
+  );
+};
+
+export default Page;
---
+++ multitenant-ecommerce/src/app/(app)/(home)/pricing/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <div>
+      <p>Pricing</p>
+    </div>
+  );
+};
+
+export default Page;
